1ca6281926b79a54f23da10cd9024954bf9eba5c
Evan Quagliato <evanquagliato@gmail.com>
2021-09-22 15:50:08 -0700

    commit

--- Class/Character.cs
+++ Class/Character.cs
@@ -1,11 +1,31 @@
 namespace RPG.Class
 {
-    public class character
+    public abstract class Character
     {
+        // 
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Health { get; set; }
         public int Attack { get; set; }
         public int Defense { get; set; }
+        public int Health { get; set; }
+
+        public int CombatRound(int enemyDamage, int damageDealt)
+        {
+            this.Health -= (enemyDamage - this.Defense);
+
+
+            return damageDealt;
+        }
+
+        // Check if the character is alive or dead
+        public bool AliveStatus()
+        {
+            if (this.Health < 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace RPG
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to this wonderful RPG");
         }
     }
 }
